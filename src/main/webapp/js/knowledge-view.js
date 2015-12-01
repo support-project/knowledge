715b33d480329f96b5a72c83938401bdf7454238
koda <koda.masaru3@gmail.com>
2015-12-01 22:21:15 +0900

    #73 コメントを折りたたんで非表示／表示を切り替えられるようにした

--- src/main/webapp/js/knowledge-view.js
+++ src/main/webapp/js/knowledge-view.js
@@ -474,4 +474,32 @@ var saveStocks = function(knowledgeId) {
 	});
 };
 
-
+var collapse = function(comment_id, collapseFlag) {
+	if (collapseFlag) {
+		$('#comment_' + comment_id).addClass('hide');
+		$('#comment_collapse_' + comment_id).removeClass('hide');
+		$('#collapse_on_' + comment_id).addClass('hide');
+		$('#collapse_off_' + comment_id).removeClass('hide');
+	} else {
+		$('#comment_' + comment_id).removeClass('hide');
+		$('#comment_collapse_' + comment_id).addClass('hide');
+		$('#collapse_on_' + comment_id).removeClass('hide');
+		$('#collapse_off_' + comment_id).addClass('hide');
+	}
+	// 保存
+	var url = _CONTEXT + '/protect.knowledge/collapse/';
+	console.log(stocksInfo);
+	$.ajax({
+		type : 'POST',
+		url : url,
+		data : 'commentNo=' + comment_id + '&collapse=' + collapseFlag,
+		scriptCharset: 'utf-8',
+		success : function(datas, dataType) {
+			console.log(datas);
+			$.notify(datas.message, 'success');
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown){
+			$.notify('[fail] collapse', 'warn');
+		}
+	});
+};
