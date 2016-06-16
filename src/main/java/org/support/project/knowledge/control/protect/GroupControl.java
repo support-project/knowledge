d02c0acc33223cb3ba075273732e75678326f040
Koda <koda.masaru3@gmail.com>
2016-06-17 03:22:45 +0900

    #399 Change number of limit on group list

--- src/main/java/org/support/project/knowledge/control/protect/GroupControl.java
+++ src/main/java/org/support/project/knowledge/control/protect/GroupControl.java
@@ -35,7 +35,7 @@ import org.support.project.web.exception.InvalidParamException;
 @DI(instance = Instance.Prototype)
 public class GroupControl extends Control {
     /** 一覧の表示件数 */
-    public static final int PAGE_LIMIT = 10;
+    public static final int PAGE_LIMIT = 20;
 
     /**
      * 自分が所属しているグループの一覧を取得
