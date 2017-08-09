9c00787991a86a6c33ef5ca8e562a1a89252ebb9
Koda <koda.masaru3@gmail.com>
2017-08-09 18:24:16 +0900

    #444 Add some necessary tables to migrate sql

--- src/main/resources/org/support/project/knowledge/deploy/v1_11_0/migrate_v1_11_1.sql
+++ src/main/resources/org/support/project/knowledge/deploy/v1_11_0/migrate_v1_11_1.sql
@@ -31,3 +31,58 @@ ALTER TABLE LIKES ADD COLUMN LIKE_CLASS integer default 1;
 
 comment on column LIKES.LIKE_CLASS is '種類';
 
+-- ユーザへの通知
+drop table if exists USER_NOTIFICATIONS cascade;
+
+create table USER_NOTIFICATIONS (
+  USER_ID INTEGER not null
+  , NO bigint not null
+  , STATUS integer
+  , ROW_ID character varying(64)
+  , INSERT_USER integer
+  , INSERT_DATETIME timestamp
+  , UPDATE_USER integer
+  , UPDATE_DATETIME timestamp
+  , DELETE_FLAG integer
+  , constraint USER_NOTIFICATIONS_PKC primary key (USER_ID,NO)
+) ;
+
+-- 通知
+drop table if exists NOTIFICATIONS cascade;
+
+create table NOTIFICATIONS (
+  NO BIGSERIAL not null
+  , TITLE VARCHAR(256)
+  , CONTENT text
+  , ROW_ID character varying(64)
+  , INSERT_USER integer
+  , INSERT_DATETIME timestamp
+  , UPDATE_USER integer
+  , UPDATE_DATETIME timestamp
+  , DELETE_FLAG integer
+  , constraint NOTIFICATIONS_PKC primary key (NO)
+) ;
+
+
+comment on table USER_NOTIFICATIONS is 'ユーザへの通知';
+comment on column USER_NOTIFICATIONS.USER_ID is 'ユーザID';
+comment on column USER_NOTIFICATIONS.NO is 'NO';
+comment on column USER_NOTIFICATIONS.STATUS is 'ステータス';
+comment on column USER_NOTIFICATIONS.ROW_ID is '行ID';
+comment on column USER_NOTIFICATIONS.INSERT_USER is '登録ユーザ';
+comment on column USER_NOTIFICATIONS.INSERT_DATETIME is '登録日時';
+comment on column USER_NOTIFICATIONS.UPDATE_USER is '更新ユーザ';
+comment on column USER_NOTIFICATIONS.UPDATE_DATETIME is '更新日時';
+comment on column USER_NOTIFICATIONS.DELETE_FLAG is '削除フラグ';
+
+comment on table NOTIFICATIONS is '通知';
+comment on column NOTIFICATIONS.NO is 'NO';
+comment on column NOTIFICATIONS.TITLE is 'タイトル';
+comment on column NOTIFICATIONS.CONTENT is 'メッセージ';
+comment on column NOTIFICATIONS.ROW_ID is '行ID';
+comment on column NOTIFICATIONS.INSERT_USER is '登録ユーザ';
+comment on column NOTIFICATIONS.INSERT_DATETIME is '登録日時';
+comment on column NOTIFICATIONS.UPDATE_USER is '更新ユーザ';
+comment on column NOTIFICATIONS.UPDATE_DATETIME is '更新日時';
+comment on column NOTIFICATIONS.DELETE_FLAG is '削除フラグ';
+
