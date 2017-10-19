aca6be15bc78b9eac88360f819c3d28b823d7005
Koda <koda.masaru3@gmail.com>
2017-10-19 16:27:30 +0900

    launch embedded tomcat

--- src/main/java/org/support/project/knowledge/Launch.java
+++ src/main/java/org/support/project/knowledge/Launch.java
@@ -1,32 +1,51 @@
 package org.support.project.knowledge;
 
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+import org.support.project.common.log.Log;
+import org.support.project.common.log.LogFactory;
+
+import javax.servlet.ServletException;
 import java.io.File;
 import java.util.Optional;
 
-import javax.servlet.ServletException;
-
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.core.StandardContext;
-import org.apache.catalina.startup.Tomcat;
-
 public class Launch {
+
+    private static final Log LOG = LogFactory.getLog(Launch.class);
+
     public static final Optional<String> PORT = Optional.ofNullable(System.getenv("PORT"));
     public static final Optional<String> HOSTNAME = Optional.ofNullable(System.getenv("HOSTNAME"));
     
     public static void main(String[] args) throws ServletException, LifecycleException {
-        String contextPath = "/knowledge" ;
-        String appBase = "/data/project/red/src/knowledge/target/knowledge";
-        Tomcat tomcat = new Tomcat();   
-        tomcat.setPort(Integer.valueOf(PORT.orElse("8080") ));
-        tomcat.setHostname(HOSTNAME.orElse("localhost"));
-        tomcat.getHost().setAppBase(appBase);
-        
-        StandardContext ctx = (StandardContext) tomcat.addWebapp(contextPath, new File(appBase).getAbsolutePath());
-        
-        
+        String webappDirLocation = "src/main/webapp/";
+        Tomcat tomcat = new Tomcat();
+
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
         tomcat.start();
         tomcat.getServer().await();
-        
     }
 
 }
