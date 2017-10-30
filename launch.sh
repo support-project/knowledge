466a393e3ffc9a9aa68e34cf59299a1222317168
Koda <koda.masaru3@gmail.com>
2017-10-31 02:21:43 +0900

    Change launch tomcat setting

--- launch.sh
+++ launch.sh
@@ -4,9 +4,8 @@ docker-compose run --rm maven mvn install -DskipTests=true -Dmaven.javadoc.skip=
 # docker-compose run --rm maven mvn clean test site -e
 docker-compose run --rm maven mvn clean package -e
 
-mv target/knowledge.war target/ROOT.war
+mkdir target/webapps
+mv target/knowledge.war target/webapps/ROOT.war
 
-docker-compose run --rm knowledge rm -rf /usr/local/tomcat/webapps/*
-docker-compose run --rm knowledge ls /usr/local/tomcat/webapps
-docker-compose up --build -d knowledge
-docker-compose logs -f knowledge
+docker-compose up --build -d tomcat
+docker-compose logs -f tomcat
