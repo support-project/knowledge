8c8b2ca236be430d01e8feee9f7c07a8d4fcf4f4
Koda <koda.masaru3@gmail.com>
2017-11-11 11:16:08 +0900

    #939 Remove deprecated methods

--- src/main/java/org/support/project/knowledge/logic/notification/webhook/CommentInsertWebhookNotification.java
+++ src/main/java/org/support/project/knowledge/logic/notification/webhook/CommentInsertWebhookNotification.java
@@ -3,11 +3,8 @@ package org.support.project.knowledge.logic.notification.webhook;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
-import java.text.SimpleDateFormat;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import org.support.project.common.log.Log;
 import org.support.project.common.log.LogFactory;
@@ -195,57 +192,5 @@ public class CommentInsertWebhookNotification {
         }
         return null;
     }
-    /**
-     * コメントのjsonデータを取得する
-     *
-     * @param comment
-     * @param knowledge
-     * @return
-     * @deprecated
-     */
-    public Map<String, Object> getCommentData(CommentsEntity comment, KnowledgesEntity knowledge) {
-        Map<String, Object> jsonObject = new HashMap<String, Object>();
-
-        /**  This code make JSON to send Slack */
-        String linktop = "<";
-        String linkpipe = "|";
-        String linkend = ">";
-        
-        /**  This code make JSON to send Slack */
-        StringBuffer SendBuff = new StringBuffer();
-        SendBuff.append(linktop);
-        SendBuff.append(NotifyLogic.get().makeURL(knowledge.getKnowledgeId()));
-        SendBuff.append(linkpipe);
-        SendBuff.append(knowledge.getTitle());
-        SendBuff.append(linkend);
-        String SendString = SendBuff.toString();
-        jsonObject.put("text", SendString);
-        
-        jsonObject.put("comment_no", comment.getCommentNo());
-        jsonObject.put("comment", comment.getComment());
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-
-        UsersEntity insertUser = UsersDao.get().selectOnKey(comment.getInsertUser());
-        if (insertUser != null) {
-            jsonObject.put("insert_user", insertUser.getUserName());
-        } else {
-            jsonObject.put("insert_user", "Unknown user");
-        }
-        jsonObject.put("insert_date", simpleDateFormat.format(comment.getInsertDatetime()));
-
-        UsersEntity updateUser = UsersDao.get().selectOnKey(comment.getInsertUser());
-        if (updateUser != null) {
-            jsonObject.put("update_user", updateUser.getUserName());
-        } else {
-            jsonObject.put("insert_user", "Unknown user");
-        }
-        jsonObject.put("update_date", simpleDateFormat.format(knowledge.getUpdateDatetime()));
-
-        jsonObject.put("knowledge", KnowledgeUpdateWebHookNotification.get().getKnowledgeData(knowledge, null));
-        return jsonObject;
-    }
-
-    
 
 }
--- src/main/java/org/support/project/knowledge/logic/notification/webhook/KnowledgeUpdateWebHookNotification.java
+++ src/main/java/org/support/project/knowledge/logic/notification/webhook/KnowledgeUpdateWebHookNotification.java
@@ -4,12 +4,9 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import org.support.project.common.log.Log;
 import org.support.project.common.log.LogFactory;
@@ -35,8 +32,6 @@ import org.support.project.knowledge.logic.WebhookLogic;
 import org.support.project.knowledge.logic.notification.QueueNotification;
 import org.support.project.knowledge.vo.notification.webhook.WebhookKnowledgeJson;
 import org.support.project.web.bean.LabelValue;
-import org.support.project.web.dao.UsersDao;
-import org.support.project.web.entity.UsersEntity;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -263,93 +258,5 @@ public class KnowledgeUpdateWebHookNotification {
         }
         return null;
     }
-
-    /**
-     * 記事のjsonデータを取得する
-     *
-     * @param knowledge
-     * @param type
-     * @return
-     * @deprecated
-     */
-    public Map<String, Object> getKnowledgeData(KnowledgesEntity knowledge, Integer type) {
-        Map<String, Object> jsonObject = new HashMap<String, Object>();
-
-        /** This code make JSON to send Slack */
-        String linktop = "<";
-        String linkpipe = "|";
-        String linkend = ">";
-
-        /** This code make JSON to send Slack */
-        StringBuffer SendBuff = new StringBuffer();
-        SendBuff.append(linktop);
-        SendBuff.append(NotifyLogic.get().makeURL(knowledge.getKnowledgeId()));
-        SendBuff.append(linkpipe);
-        SendBuff.append(knowledge.getTitle());
-        SendBuff.append(linkend);
-        String SendString = SendBuff.toString();
-        jsonObject.put("text", SendString);
-
-        jsonObject.put("knowledge_id", knowledge.getKnowledgeId());
-        jsonObject.put("title", knowledge.getTitle());
-        jsonObject.put("content", knowledge.getContent());
-        jsonObject.put("public_flag", knowledge.getPublicFlag());
-        jsonObject.put("like_count", knowledge.getLikeCount());
-        jsonObject.put("comment_count", knowledge.getCommentCount());
-        jsonObject.put("type_id", knowledge.getTypeId());
-        jsonObject.put("link", NotifyLogic.get().makeURL(knowledge.getKnowledgeId()));
-
-        boolean became_public = false;
-        if (knowledge.getNotifyStatus() == null || knowledge.getNotifyStatus().intValue() == 0) {
-            if (knowledge.getPublicFlag().intValue() != KnowledgeLogic.PUBLIC_FLAG_PRIVATE) {
-                // 今まで非公開だったものが、初めての通知になった
-                became_public = true;
-            }
-        }
-        jsonObject.put("became_public", became_public);
-
-        if (type != null) {
-            if (QueueNotification.TYPE_KNOWLEDGE_INSERT == type) {
-                jsonObject.put("status", "created");
-            } else {
-                jsonObject.put("status", "updated");
-            }
-        }
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-
-        UsersEntity insertUser = UsersDao.get().selectOnKey(knowledge.getInsertUser());
-        if (insertUser != null) {
-            jsonObject.put("insert_user", insertUser.getUserName());
-        } else {
-            jsonObject.put("insert_user", "Unknown user");
-        }
-        jsonObject.put("insert_date", simpleDateFormat.format(knowledge.getInsertDatetime()));
-
-        UsersEntity updateUser = UsersDao.get().selectOnKey(knowledge.getInsertUser());
-        if (updateUser != null) {
-            jsonObject.put("update_user", updateUser.getUserName());
-        } else {
-            jsonObject.put("insert_user", "Unknown user");
-        }
-        jsonObject.put("update_date", simpleDateFormat.format(knowledge.getUpdateDatetime()));
-
-        List<TagsEntity> tagsEntities = TagsDao.get().selectOnKnowledgeId(knowledge.getKnowledgeId());
-        List<String> tags = new ArrayList<String>();
-        for (TagsEntity tag : tagsEntities) {
-            tags.add(tag.getTagName());
-        }
-        jsonObject.put("tags", tags);
-
-        List<LabelValue> labelGroups = TargetLogic.get().selectTargetsOnKnowledgeId(knowledge.getKnowledgeId());
-        List<String> groups = new ArrayList<String>();
-        for (LabelValue label : labelGroups) {
-            if (label.getValue().startsWith("G")) {
-                groups.add(label.getLabel());
-            }
-        }
-        jsonObject.put("groups", groups);
-
-        return jsonObject;
-    }
+    
 }
