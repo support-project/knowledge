b4581ef1cb46304f2650fc61788bcb64d81c7412
y-ueda <y-ueda@cmknet.co.jp>
2020-07-08 11:43:45 +0900

    refs #7 フォーマッター適用(動作検証の為実施)

--- src/main/java/org/support/project/aop/Advice.java
+++ src/main/java/org/support/project/aop/Advice.java
@@ -2,10 +2,9 @@ package org.support.project.aop;
 
 import java.lang.reflect.Method;
 
-
 /**
  * メソッドが呼ばれた時に呼ばれる
- * 
+ *
  */
 public interface Advice<T> {
 	Object invoke(T object, Method method, Object[] args) throws Throwable;
