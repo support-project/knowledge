bbbbb70f45f18b19ec6239c7f19dbaefeab79859
Olaf Meeuwissen <Meeuwissen.Olaf@exc.epson.co.jp>
2021-02-01 14:24:47 +0900

    Propagate proxy settings to maven invocations

--- launch.sh
+++ launch.sh
@@ -1,8 +1,25 @@
 #!/bin/bash -eu
 
-docker-compose run --rm maven mvn install -DskipTests=true -Dmaven.javadoc.skip=true -e
-# docker-compose run --rm maven mvn clean test site -e
-docker-compose run --rm maven mvn clean package -e
+propagate_proxy_vars () {
+  # For newer JVM versions, use this instead.
+  # echo " -Djava.net.useSystemProxies=true"
+
+  if test -n "$https_proxy"; then
+    echo " -Dhttps.proxyHost=$(echo $https_proxy | sed 's|.*://||; s|:.*||')"
+    echo " -Dhttps.proxyPort=$(echo $https_proxy | sed 's|.*:||')"
+  fi
+  if test -n "$http_proxy"; then
+    echo " -Dhttp.proxyHost=$(echo $http_proxy | sed 's|.*://||; s|:.*||')"
+    echo " -Dhttp.proxyPort=$(echo $http_proxy | sed 's|.*:||')"
+  fi
+  if test -n "$no_proxy"; then
+    echo " -Dhttp.nonProxyHosts=$(echo $no_proxy | sed 's/,\./,*./g; s/,/|/g')"
+  fi
+}
+
+docker-compose run --rm maven mvn install -DskipTests=true -Dmaven.javadoc.skip=true -e $(propagate_proxy_vars)
+# docker-compose run --rm maven mvn clean test site -e $(propagate_proxy_vars)
+docker-compose run --rm maven mvn clean package -e $(propagate_proxy_vars)
 
 mkdir target/webapps
 mv target/knowledge.war target/webapps/ROOT.war
