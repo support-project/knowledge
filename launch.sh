f3e1d694f044f43bf8fb92a51587f32c5c290921
y-okumura-isp <y-okumura@isp.co.jp>
2021-03-27 17:14:40 +0900

    Use forked Markedj (#1089)

--- launch.sh
+++ launch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eu
 
+mkdir -p third_party
+git clone --branch v1-dev https://github.com/y-okumura-isp/markedj.git third_party/markedj
+
+docker-compose run --rm maven sh -c "cd third_party/markedj; mvn install -DskipTests=true -Dmaven.javadoc.skip=true"
 docker-compose run --rm maven mvn install -DskipTests=true -Dmaven.javadoc.skip=true -e
 # docker-compose run --rm maven mvn clean test site -e
 docker-compose run --rm maven mvn clean package -e
